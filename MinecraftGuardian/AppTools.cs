d773188e044c5decf0cbeec0aa671c4feae91a27
Alex Cai <yuting09120310@gmail.com>
2024-06-18 17:39:29 +0800

    備份 + log

---
+++ MinecraftGuardian/AppTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MinecraftGuardian
+{
+    public class AppTools
+    {
+
+        public static void Log(string action, bool success)
+        {
+            string LogDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
+            if (!Directory.Exists(LogDirectory))
+            {
+                Directory.CreateDirectory(LogDirectory);
+            }
+
+
+            string logFilePath = Path.Combine(LogDirectory, $"{DateTime.Now:yyyy-MM-dd}.txt");
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} {action} {(success ? "成功" : "失敗")}";
+
+            try
+            {
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"無法寫入日誌: {ex.Message}");
+            }
+        }
+    }
+}
--- MinecraftGuardian/Form1.cs
+++ MinecraftGuardian/Form1.cs
@@ -1,8 +1,11 @@
 using System.Diagnostics;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace MinecraftGuardian
 {
@@ -13,12 +16,10 @@ namespace MinecraftGuardian
         private string processName = Program.Configuration["ProcessName"]!;  // 假設啟動的進程是 Java
         private string startCommand = Program.Configuration["StartApp"]!;  // 啟動 start.bat 的路徑
 
-
         public Form1()
         {
             InitializeComponent();
         }
-
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
@@ -28,9 +29,9 @@ namespace MinecraftGuardian
                 monitorThread = new Thread(MonitorProcess);
                 monitorThread.Start();
                 labelStatus.Text = "監控中.";
+                AppTools.Log("監控開始", true);
             }
         }
-
 
         private void buttonStop_Click(object sender, EventArgs e)
         {
@@ -39,43 +40,54 @@ namespace MinecraftGuardian
                 monitoring = false;
                 monitorThread.Join();
                 labelStatus.Text = "監控停止.";
+                AppTools.Log("監控停止", true);
             }
         }
-
 
         private async void MonitorProcess()
         {
             while (monitoring)
             {
-                if (!IsProcessRunning(processName))
+                try
+                {
+                    if (!IsProcessRunning(processName))
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            labelStatus.Text = $"{processName} 未運行。正在啟動...";
+                        });
+
+                        StartProcess(startCommand);
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            labelStatus.Text = $"{processName} 啟動完成.";
+                        });
+                        AppTools.Log($"{processName} 啟動", true);
+                    }
+                    else
+                    {
+                        Invoke((MethodInvoker)delegate
+                        {
+                            labelStatus.Text = $"{processName} 已啟動";
+                        });
+                    }
+
+                    // 如果是指定時間就去做地圖備份
+                    await BackupMapAsync();
+
+                    await Task.Delay(10000);  // 每 10 秒檢查一次
+                }
+                catch (Exception ex)
                 {
                     Invoke((MethodInvoker)delegate
                     {
-                        labelStatus.Text = $"{processName} 未運行。正在啟動...";
+                        labelStatus.Text = $"監控過程中發生錯誤: {ex.Message}";
                     });
-
-                    StartProcess(startCommand);
-
-                    Invoke((MethodInvoker)delegate
-                    {
-                        labelStatus.Text = $"{processName} 啟動完成.";
-                    });
+                    AppTools.Log("監控過程中發生錯誤", false);
                 }
-                else
-                {
-                    Invoke((MethodInvoker)delegate
-                    {
-                        labelStatus.Text = $"{processName} 已啟動";
-                    });
-                }
-
-                // 如果是指定時間就去做地圖備份
-                await BackupMapAsync();
-
-                await Task.Delay(10000);  // 每 10 秒檢查一次
             }
         }
-
 
         private bool IsProcessRunning(string processName)
         {
@@ -83,12 +95,12 @@ namespace MinecraftGuardian
             return processes.Length > 0;
         }
 
-
         private void StartProcess(string command)
         {
             try
             {
                 Process.Start(command);
+                AppTools.Log($"啟動 {command}", true);
             }
             catch (Exception ex)
             {
@@ -96,40 +108,72 @@ namespace MinecraftGuardian
                 {
                     labelStatus.Text = $"啟動失敗 原因: {ex.Message}";
                 });
+                AppTools.Log($"啟動 {command} 失敗", false);
             }
         }
-
 
         /// <summary>
         /// 備份地圖
         /// </summary>
         private async Task BackupMapAsync()
         {
-            DateTime dateTime = DateTime.Now;
-            if(dateTime.Hour == 6 || dateTime.Hour == 18 && dateTime.Minute == 0 && dateTime.Second == 0)
+            try
             {
-                labelStatus.Text = "地圖備份中...";
-
-                //取得所有進程
-                Process[] processes = Process.GetProcessesByName("java");
-                processes.ToList().ForEach(p =>
+                DateTime dateTime = DateTime.Now;
+                if ((dateTime.Hour == 0|| dateTime.Hour == 6 || dateTime.Hour == 12 || dateTime.Hour == 18) && dateTime.Minute == 0 && dateTime.Second == 0)
                 {
-                    p.Kill();
-                });
+                    Invoke((MethodInvoker)delegate
+                    {
+                        labelStatus.Text = "地圖備份中...";
+                    });
+                    AppTools.Log("地圖備份開始", true);
 
-                string mapPath = Program.Configuration["MapPath"]!;
-                string backupPath = Program.Configuration["BackupPath"]!;
-                string zipFileName = $"backup_{DateTime.Now:yyyyMMddHHmm}.zip";
-                string zipFilePath = Path.Combine(backupPath, zipFileName);
+                    //取得所有進程
+                    Process[] processes = Process.GetProcessesByName("java");
+                    processes.ToList().ForEach(p =>
+                    {
+                        try
+                        {
+                            p.Kill();
+                            AppTools.Log($"終止進程: {p.ProcessName} (PID: {p.Id})", true);
+                        }
+                        catch (Exception ex)
+                        {
+                            Invoke((MethodInvoker)delegate
+                            {
+                                labelStatus.Text = $"無法終止進程: {p.ProcessName} (PID: {p.Id})。錯誤: {ex.Message}";
+                            });
+                            AppTools.Log($"無法終止進程: {p.ProcessName} (PID: {p.Id})", false);
+                        }
+                    });
 
-                if (!Directory.Exists(backupPath))
-                {
-                    Directory.CreateDirectory(backupPath);
+                    string mapPath = Program.Configuration["MapPath"]!;
+                    string backupPath = Program.Configuration["BackupPath"]!;
+                    string zipFileName = $"backup_{DateTime.Now:yyyyMMddHHmm}.zip";
+                    string zipFilePath = Path.Combine(backupPath, zipFileName);
+
+                    if (!Directory.Exists(backupPath))
+                    {
+                        Directory.CreateDirectory(backupPath);
+                    }
+
+                    ZipFile.CreateFromDirectory(mapPath, zipFilePath);
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        labelStatus.Text = "地圖備份完成.";
+                    });
+                    AppTools.Log("地圖備份完成", true);
                 }
-
-                ZipFile.CreateFromDirectory(mapPath, zipFilePath);
+            }
+            catch (Exception ex)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    labelStatus.Text = $"備份過程中發生錯誤: {ex.Message}";
+                });
+                AppTools.Log("備份過程中發生錯誤", false);
             }
         }
-
     }
 }
